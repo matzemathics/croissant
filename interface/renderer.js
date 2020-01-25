38f1ff358bdc7d5ac083ea56ebb00f01ab76a52e
matzemathics <geige.matze@gmail.com>
2020-01-25 17:52:22 +0100

    content

---
+++ interface/renderer.js
@@ -0,0 +1,17 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// No Node.js APIs are available in this process because
+// `nodeIntegration` is turned off. Use `preload.js` to
+// selectively enable features needed in the rendering
+// process.
+
+window.addEventListener('load', () => {
+    window.audio.init(() => { console.log('callback') });
+    window.audio.play();
+    console.log(document.getElementById('play'))
+    document.getElementById('play').contentDocument.onclick = () => {
+        window.audio.play();
+    }
+    console.log(document.getElementById("cover-image").src)
+})
+
